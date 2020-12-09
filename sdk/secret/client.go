eda16dca658dc992871741e960df94237d1d0543
Nilsas Firantas <n.firantas@gmail.com>
2020-12-09 21:01:38 +0200

    add crud operations

--- sdk/secret/client.go
+++ sdk/secret/client.go
@@ -44,3 +44,42 @@ func (r *Client) GetPassword(ctx context.Context, id int) (*[]SecretResponse, er
 	}
 	return &resp, nil
 }
+
+// SetPassword Updates password in place
+func (r *Client) SetPassword(ctx context.Context, pass *Secret) (*SecretResponse, error) {
+	j, err := json.Marshal(pass)
+	if err != nil {
+		return nil, err
+	}
+	raw, code, err := r.put(ctx, "/api/passwords", nil, j)
+	if err != nil {
+		return nil, err
+	}
+	var resp SecretResponse
+	err = json.Unmarshal(raw, &resp)
+	if err != nil {
+		return nil, err
+	}
+	if code != 200 {
+		return nil, fmt.Errorf("HTTP error %d: returns %s", code, resp.Status)
+	}
+	return &resp, err
+}
+
+// DeletePassword removes password from the passwordlist specified
+func (r *Client) DeletePassword(ctx context.Context, pass *Secret) error {
+	var uri string
+	if pass.MoveToRecycleBin == true {
+		uri = fmt.Sprintf("/api/passwords/%d?MoveToRecycleBin=%s", pass.PasswordID, "True")
+	} else {
+		uri = fmt.Sprintf("/api/passwords/%d?MoveToRecycleBin=%s", pass.PasswordID, "False")
+	}
+
+	_, code, err := r.delete(ctx, uri)
+	if err != nil {
+		return err
+	}
+	if code != 204 {
+		return fmt.Errorf("HTTP error %d: expected status 204 No content")
+	}
+}
